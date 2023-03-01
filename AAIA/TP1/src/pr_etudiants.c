255a1c71405dea8b560dcc4384d6c1f15816edf1
Swan <maillard.swan@gmail.com>
2023-03-01 17:16:48 +0100

    TP1 géré comme des boss

--- AAIA/TP1/src/pr_etudiants.c
+++ AAIA/TP1/src/pr_etudiants.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef struct{
     int n; // number of vertices in the graph
@@ -8,6 +9,10 @@ typedef struct{
     int** succ; // for each 0<=i<n and each 0<=j<nbSucc[i], succ[i][j]=jth successor of i
 } DIGRAPH;
 
+typedef struct{
+    int page;
+    double proba;
+} Pair;
 
 DIGRAPH* readDigraph(FILE *fp){
     // return the DIGRAPH contained in file fp
@@ -45,10 +50,111 @@ void printDigraph(DIGRAPH *g){
     }
 }
 
-int main(){
-    FILE* fp  = fopen("exemple1.txt", "r");
+void computeProba(double* sk, DIGRAPH* g, double alpha) {
+    double* skprev = malloc(sizeof(double)*g->n);
+    
+    for(int i = 0 ; i<=g->n ; i++){
+        skprev[i] = sk[i];
+        sk[i] = 0;
+    }
+
+    double qabs = 0;
+    for (int i = 0; i<g->n; i++)
+        if (g->nbSucc[i] == 0)
+            qabs += skprev[i];
+    qabs /= g->n;
+
+    double q = (1.-alpha) / g->n ;
+
+    for(int i = 0 ; i<g->n ; i++) {
+        for(int j = 0 ; j<g->nbSucc[i] ; j++)
+            sk[g->succ[i][j]] += alpha * (skprev[i]) / (g->nbSucc[i]);
+
+        sk[i] += alpha*qabs + q;
+    }
+
+    free(skprev);
+}
+
+void printsk(int k, double* sk, int size) {
+    printf("s%d = ( ", k);
+    double somme = 0;
+    for (int i = 0; i < size; i++){
+        printf("%lf ", sk[i]);
+        somme += sk[i];
+    }
+    printf(") ; S%d = %lf\n", k, somme);
+    
+}
+
+void findTopPages(double * sk, int size) {
+    Pair topPages[5];
+    for(int i = 0 ; i<5 ; i++)
+        topPages[i].proba = 0;
+    
+
+    for(int i = 0 ; i<size ; i++){
+        int j = 4;
+        if(topPages[j].proba < sk[i]){   
+            j--;
+            while(j >= 0 && topPages[j].proba < sk[i])
+            {
+                j--;
+            }
+
+            for (int k = 4; k > j; k--)
+                topPages[k] = topPages[k-1];
+
+            topPages[j+1].page = i;
+            topPages[j+1].proba = sk[i];
+        }
+    }
+    for(int i = 0 ; i<5 ; i++){
+        printf("Top %d: %d\n", i+1, topPages[i].page);
+    }
+}
+
+int main() {
+    FILE* fp  = fopen("genetic.txt", "r");
     DIGRAPH* g = readDigraph(fp);
+    double alpha = 0.9;
     fclose(fp);
     printDigraph(g);
+
+    double * sk = malloc(sizeof(double)*g->n);
+    double * skprev = malloc(sizeof(double)*g->n);
+    for (int i=0; i<g->n; ++i)
+        sk[i] = 1./g->n;
+    //printsk(0, sk, g->n);
+
+    double epsilon = pow(10, -10);
+    int i = 0;
+    int convergence = 0;
+    do {
+        for (int j=0; j<g->n; ++j)
+            skprev[j] = sk[j];
+
+        computeProba(sk, g, alpha);
+        //printsk(i, sk, g->n);
+
+        for (int j=0; j<g->n; ++j) {
+            convergence = 1;
+            if (fabs(sk[j] - skprev[j]) > epsilon) {
+                convergence = 0;
+                break;
+            }
+        }
+
+        ++i;
+    }
+    while (!convergence);
+    
+    printf("Convergence à k = %d\n", i);
+    findTopPages(sk, g->n);
+    
+
+    free(sk);
+    free(skprev);
+
     return 0;
 }
