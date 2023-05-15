8e92ba52f348bb87023ee9a1ed404239f4cef756
Swan <maillard.swan@gmail.com>
2023-05-15 17:39:42 +0200

    Proba

--- Proba/TP/FileMM1.R
+++ Proba/TP/FileMM1.R
@@ -1,6 +1,7 @@
 FileMM1 <- function(lambda, mu, D) {
   arrivee <- c()
   depart <- c()
+  attente_service <- c()
   
   temps_arrivee <- 0
   temps_depart <- 0
@@ -15,15 +16,18 @@ FileMM1 <- function(lambda, mu, D) {
       duree_attente <- rexp(1, mu)
       if (temps_arrivee > temps_depart)
       {
+        temps_attente_service <- 0;
         temps_depart <- temps_arrivee + duree_attente
       }
       else 
       {
+        temps_attente_service <- temps_depart - temps_arrivee
         temps_depart <- temps_depart + duree_attente
       }
       depart <- c(depart, temps_depart)
+      attente_service <- c(attente_service, temps_attente_service)
     }
   }
   
-  return(list(arrivee = arrivee, depart = depart))
+  return(list(arrivee = arrivee, depart = depart, attente_service = attente_service))
 }
